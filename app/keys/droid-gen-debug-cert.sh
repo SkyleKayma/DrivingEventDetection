59b7cdb2976f1e9300c17971340223e4a8630613
Ludovic Godart <l.godart@openium.fr>
2020-10-08 18:08:16 +0200

    Removed .idea files and folder

--- app/keys/droid-gen-debug-cert.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-keytool -genkey -v -keystore debug.keystore -alias androiddebugkey -storepass android -keypass android -keyalg RSA -validity 14000
--- app/keys/droid-gen-release-cert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ $# -lt 1 ]; then
-    echo "Usage: $0 alias"
-    exit 1
-fi
-
-keytool -genkey -v -keystore release.keystore -alias $1 -keyalg RSA -keysize 2048 -validity 10000
